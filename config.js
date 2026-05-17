dd74e7a3c55c03248a5d4b2f1c0adbae7aac57b4
Adyaaaa01 <adyaasd1@gmail.com>
2026-05-17 12:53:02 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Production configuration
+const DEMO_MODE = false;
+
+const SUPABASE_URL = "https://wgxzzxmwqdtofootjvtq.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_yObMV88bcdo3F4x-3f_5zQ_xfyy3SFP";
+
+const APP_NAME = "Гэр бүлийн Санхүү";
